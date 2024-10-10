745ea365432583844030e75aed195db13afac094
kaeht <maxgacias@gmail.com>
2024-10-10 16:07:17 +0200

    First Commit Calculator

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+//importar
+import React from "react";
+import './App.css';
+
+// generar funcionalidad (funcion flecha)
+const App  =  () => {
+    //Lo que hace la funcion
+    console.log("Renderizacion de la App");
+    return (
+    <main className="react-calculator">
+        <div className="result">
+
+        </div>
+        <div className="numbers">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+        </div>
+        <div className="functions">
+             <button>
+                clear
+             </button>
+             <button>
+                r
+             </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+    </main>
+    )
+}
+
+//exportacion
+export default App
